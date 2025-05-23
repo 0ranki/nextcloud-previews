7c04693517f38434da0284ecad43a7d3984f77a0
Jarno Rankinen <jarno@oranki.net>
2025-05-23 12:38:18 +0300

    Fix entrypoint script su command

--- dbup.sh
+++ dbup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-su -c '/var/www/html/occ db:add-missing-indices' - www-data
-su -c '/var/www/html/occ maintenance:repair --include-expensive' - www-data
+su -s /bin/bash -c '/var/www/html/occ db:add-missing-indices' www-data
+su -s /bin/bash -c '/var/www/html/occ maintenance:repair --include-expensive' www-data
